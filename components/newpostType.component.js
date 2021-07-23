e330a3b683299130f1180be0726f37bedc088e13
NganVanLuyen <nganvanluyen@gmail.com>
2021-07-23 22:43:40 +0700

    add screens whoconfirm and newpostType

---
+++ components/newpostType.component.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+
+export default function NewPostType(props) {
+  const { newpost } = props;
+  return (
+    <TouchableOpacity
+      activeOpacity={0.3}>
+      <View style={styles.container}>
+        <Text style={styles.title}>{newpost.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    shadowOffset: { width: 0, height: 0 },
+    marginTop: 10,
+  },
+  title: {
+    fontWeight: '700',
+    fontSize: 18,
+  }
+})
