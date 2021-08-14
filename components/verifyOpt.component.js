896c03c1a77332a4a7bcc1197749846ec72e3513
NganVanLuyen <nganvanluyen@gmail.com>
2021-08-14 22:12:12 +0700

    adjust optScreen

--- components/verifyOpt.component.js
+++ components/verifyOpt.component.js
@@ -1,15 +1,53 @@
 import React, { useState } from "react";
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
-// import OTPInputView from "@twotalltotems/react-native-otp-input";
+// if (Platform.OS === "ios") {
+//   import OTPInputView from "@twotalltotems/react-native-otp-input";
+// } else {
+//   import OtpInputs from "react-native-otp-inputs";
+// }
+
+import OTPInputView from "@twotalltotems/react-native-otp-input";
+import OtpInputs from "react-native-otp-inputs";
+
 import logoSmai from "../assets/logo_smai.png";
 
 export default function VerifyOtp(props) {
+  let optInput;
+  if (Platform.OS === "ios") {
+    //switch for ios
+    optInput = (
+      <OTPInputView
+        handleChange={(code) => console.log(code)}
+        numberOfInputs={6}
+        style={styles.optStyle}
+        inputStyles={styles.optInput}
+      />
+    );
+  } else {
+    //check box
+    optInput = (
+      <OtpInputs
+        handleChange={(code) => console.log(code)}
+        numberOfInputs={6}
+        style={styles.optStyle}
+        inputStyles={styles.optInput}
+      />
+    );
+  }
+
   return (
     <View style={styles.wrapContent}>
       <Image source={logoSmai} style={styles.styleImg} />
       <Text style={styles.styleText}>Nhập mã OTP</Text>
+      {optInput}
       {/* <OTPInputView
+        handleChange={(code) => console.log(code)}
+        numberOfInputs={6}
+        style={styles.optStyle}
+        inputStyles={styles.optInput}
+      /> */}
+      {/* <OtpInputs
         handleChange={(code) => console.log(code)}
         numberOfInputs={6}
         style={styles.optStyle}
@@ -60,6 +98,7 @@ const styles = StyleSheet.create({
   },
   optInput: {
     borderWidth: 0.5,
+    // borderColor: "red",
     borderRadius: 5,
     marginHorizontal: 5,
     paddingHorizontal: 4,
